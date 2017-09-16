ceaeaac0b6ca040e05d535f30c7323e625390cb9
Ivey Topaz <iveytopaz@gmail.com>
2017-09-16 15:16:35 +0100

    Center content

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import Background from '../components/Background';
+import Preview from '../components/Preview';
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+};
 
 class HomePage extends Component {
   render() {
     return (
-      <div>
+      <div style={styles.container}>
         <Background />
+          <Preview />
       </div>
     );
   }
 }
 
-export default HomePage;
+export default Radium(HomePage);
