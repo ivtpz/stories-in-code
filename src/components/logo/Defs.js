27c15fc369a0ca64b0cb08593408b0b9c849b5d5
Ivey Topaz <iveytopaz@gmail.com>
2017-09-16 15:16:22 +0100

    Get rid of width and height

--- src/components/logo/Defs.js
+++ src/components/logo/Defs.js
@@ -1,21 +1,14 @@
 import React from 'react';
 
-const Defs = () =>
-  (<svg>
+const Defs = () => (
+  <svg width="0" height="0">
     <defs>
       <filter id="filter" height="2" width="2">
-        <feTurbulence
-          baseFrequency="0.5"
-          numOctaves="10"
-          type="fractalNoise"
-        />
-        <feDisplacementMap
-          scale="1"
-          xChannelSelector="R"
-          in="SourceGraphic"
-        />
+        <feTurbulence baseFrequency="0.5" numOctaves="10" type="fractalNoise" />
+        <feDisplacementMap scale="1" xChannelSelector="R" in="SourceGraphic" />
       </filter>
     </defs>
-  </svg>);
+  </svg>
+);
 
 export default Defs;
